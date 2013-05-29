d0297a92a83e224be4bddb1b3d783529b62a0889
Jennifer Eliuk <jen7maples@gmail.com>
2013-05-29 13:35:29 -0600

    initial commit, new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatientIntake::Application.config.secret_token = '64a4159d9516cab71e2fc76cc87902b208855f6e08343ffd1120a9dd2579c0ae58a2df23d917113165fb1c9fb336320f57220f6a91c767931a6fea4b65811198'
