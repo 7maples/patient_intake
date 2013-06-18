2b5da9e218cdd0127104c7f25bd7041f50990338
Jennifer Eliuk <jen7maples@gmail.com>
2013-06-18 10:33:02 -0600

    Reorganize specs with Cory F to follow convention

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    id_str "MyString"
+    screen_name "MyString"
+    name "MyString"
+    profile_image_url "MyString"
+  end
+end
